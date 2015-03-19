2071aa3736d48331f5631504ba90bc7dfce7b452
edisonarango <edisonarango@mbp-de-edison>
2015-03-19 20:38:38 -0300

    Agregado servlet

---
+++ src/java/VerificarCuadrado.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author edisonarango
+ */
+@WebServlet(urlPatterns = {"/VerificarCuadrado"})
+public class VerificarCuadrado extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet VerificarCuadrado</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            //out.println("<h1>Servlet VerificarCuadrado at " + request.getContextPath() + "</h1>");
+            String[][] nombres={{"0","1","2"},{"3","4","5"},{"6","7","8"}};
+            int [][] parametros= new int [3][3];
+            principal:
+            for (int i = 0; i < parametros.length; i++) {
+                for (int j = 0; j < parametros[0].length; j++) {
+                    String parametro = request.getParameter(nombres[i][j]);
+                    if (parametro=="") {
+                        out.print("<center>Hay elementos vac&iacute;os</center>");
+                        break principal;
+                    }
+                    else{
+                        parametros[i][j]=Integer.valueOf(parametro);
+                    }
+                }
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/index.jsp
+++ web/index.jsp
@@ -9,32 +9,45 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script> 
         <title>Cuadrado M&aacute;gico</title>
         <link rel="stylesheet" href="Estilos.css"/>
     </head>
     <body>
         <center>
         <br><br><br><br>
-        <form method="get" action="EcuacionSegundoGrado">           
+        <form method="get" action="VerificarCuadrado" target="frameVerificacion">           
             <div style="border: 1px solid;width: 178px;border-color: #DCDCDC;">
-            <input class="cuadrado" type="text" name="0">
-            <input class="cuadrado" type="text" name="1">
-            <input class="cuadrado" type="text" name="2">
-            <br>
-            <input class="cuadrado" type="text" name="3">
-            <input class="cuadrado" type="text" name="4">
-            <input class="cuadrado" type="text" name="5">
-            <br>
-            <input class="cuadrado" type="text" name="6">
-            <input class="cuadrado" type="text" name="7">
-            <input class="cuadrado" type="text" name="8">
-            <br>
-            </div>
-            <input type="submit" value="Verificar">
-            <input type="submit" onclick="borrar()" value="Borrar">
+                <input class="cuadrado solo-numero" type="text" maxlength="1" name="0">
+                <input class="cuadrado solo-numero" type="text" maxlength="1" name="1">
+                <input class="cuadrado solo-numero" type="text" maxlength="1" name="2">
+                <br>
+                <input class="cuadrado solo-numero" type="text" maxlength="1" name="3">
+                <input class="cuadrado solo-numero" type="text" maxlength="1"name="4">
+                <input class="cuadrado solo-numero" type="text" maxlength="1" name="5">
+                <br>
+                <input class="cuadrado solo-numero" type="text" maxlength="1" name="6">
+                <input class="cuadrado solo-numero" type="text" maxlength="1" name="7">
+                <input class="cuadrado solo-numero" type="text" maxlength="1" name="8">
+                <br>
+                </div>
+                <input type="submit" value="Verificar">
         </form>
         <button onclick="borrar()">Borrar</button>
+        <br>
         <iframe style="width: 920px;height: 55px" id="frameVerificacion"></iframe>
     </center>
+    
+    <script>
+        $(document).ready(function (){
+          $('.solo-numero').keyup(function (){
+            this.value = (this.value + '').replace(/[^0-9]/g, '');
+          });
+        });
+        
+        function borrar() {
+            $(".cuadrado").val('');
+        }
+    </script>
     </body>
 </html>
